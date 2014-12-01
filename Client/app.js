67230e3f6320f9ea1e6276eb291906ae001ea0b5
Christopher Treviño <christophertrev@gmail.com>
2014-12-01 15:20:00 -0800

    Added directive draw instead of canvas.js

--- Client/app.js
+++ Client/app.js
@@ -10,6 +10,79 @@ angular.module('ct-draw',[
 //         controller:
 //       })
 // })
-.controller('stats', function ($scope){
-  $scope.color = '(red, blue, green)';
+.controller('stats', function ($scope, $document){
+// var path;
+//     var drag = false;
+
+//     $scope.mouseUp = function(){
+//         //Clear Mouse Drag Flag
+//         drag = false;
+//     };
+
+//     $scope.mouseDrag = function(event){
+//         if(drag){
+//             path.add(new paper.Point(event.x, event.y));
+//             path.smooth();
+//         }
+//     };
+
+//     $scope.mouseDown = function(event){
+//         //Set  flag to detect mouse drag
+//         drag = true;
+//         path = new paper.Path();
+//         path.strokeColor = 'black';
+//         path.add(new paper.Point(event.x, event.y));
+//     };
+
+//     init();
+//     function init(){
+//         paper.install(window);
+//         paper.setup('myCanvas');          
+//     }
 })
+.directive('draw', function () {
+    return {
+        restrict: 'E',
+        scope: {
+
+        },
+        template: '<canvas id="myCanvas" ></canvas>',
+        replace: true,
+        link: function postLink(scope, element, attrs) {
+            var path;
+            var drag = false;
+
+            function mouseUp(event) {
+                //Clear Mouse Drag Flag
+                drag = false;
+
+            }
+
+            function mouseDrag(event) {
+                if (drag) {
+                    path.add(new paper.Point(event.layerX, event.layerY));
+                    path.smooth();
+                }
+            }
+
+            function mouseDown(event) {
+                //Set  flag to detect mouse drag
+                drag = true;
+                path = new paper.Path();
+                path.strokeColor = 'black';
+                path.add(new paper.Point(event.layerX, event.layerY));
+            }
+
+            function initPaper() {
+                paper.install(window);
+                paper.setup('myCanvas');
+            }
+
+            element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
+
+            initPaper();
+
+        }
+    };
+});
+
--- Client/canvas.js
+++ Client/canvas.js
@@ -1,18 +1,18 @@
-window.onload = function () {
-  paper.setup('myCanvas');
-  var p = paper;
-  var tool = new p.Tool();
-  console.log(p.view)
-  // path = new p.Path();
-  // path.strokeColor = 'black';
-  tool.onMouseDown= function (event) {
-      path = new p.Path();
-      path.strokeColor = 'black';
-      path.add(event.point);
-    }
-  tool.onMouseDrag = function(event) {
-    // Use the arcTo command to draw cloudy lines
-    path.arcTo(event.point);
-  }
-  tool.activate();
-}
+// window.onload = function () {
+//   paper.setup('myCanvas');
+//   var p = paper;
+//   var tool = new p.Tool();
+//   // console.log(p.view)
+//   // path = new p.Path();
+//   // path.strokeColor = 'black';
+//   tool.onMouseDown= function (event) {
+//       path = new p.Path();
+//       path.strokeColor = 'black';
+//       path.add(event.point);
+//     }
+//   tool.onMouseDrag = function(event) {
+//     // Use the arcTo command to draw cloudy lines
+//     path.arcTo(event.point);
+//   }
+//   tool.activate();
+// }
