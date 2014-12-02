31ce5f6317f13592000817ac3f13bbb41d8a7bce
Christopher Treviño <christophertrev@gmail.com>
2014-12-01 19:44:27 -0800

    Add socket io and http to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var morgan = require('morgan')
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 3000))
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/../Client'));
 
-module.exports = app;
+module.exports = function(){
+  http.listen(app.get('port'), function(){
+    console.log('listening on ', app.get('port'));
+  });  
+};
 // app.listen(3000);
