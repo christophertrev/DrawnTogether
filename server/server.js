7759cf6560483d911b924e04dd4995e748379b05
Christopher Treviño <christophertrev@gmail.com>
2014-12-01 20:35:51 -0800

    Websockets function properly

--- server/server.js
+++ server/server.js
@@ -9,6 +9,21 @@ app.set('port', (process.env.PORT || 3000))
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/../Client'));
 
+io.on('connection',function (socket){
+  socket.on('pointer',function(loc){
+    // console.log('locaation ', loc);
+    socket.broadcast.emit('drag', loc);
+  });
+  socket.on('start',function(loc){
+    console.log('start: ',loc)
+    socket.broadcast.emit('start', loc)
+  })
+})
+
+
+
+
+
 module.exports = function(){
   http.listen(app.get('port'), function(){
     console.log('listening on ', app.get('port'));
