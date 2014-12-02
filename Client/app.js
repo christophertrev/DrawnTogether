450c142708ab6d2543c6f873b5d6b4fbddad701e
Christopher Treviño <christophertrev@gmail.com>
2014-12-01 16:05:51 -0800

    add deactivate button

--- Client/app.js
+++ Client/app.js
@@ -11,44 +11,45 @@ angular.module('ct-draw',[
 //       })
 // })
 .controller('stats', function ($scope, $document){
-            var path;
-            var drag = false;
-            // console.log('scope',scope)
-            function mouseUp(event) {
-                //Clear Mouse Drag Flag
-                drag = false;
+  var path;
+  var drag = false;
+  function mouseUp(event) {
+      //Clear Mouse Drag Flag
+      drag = false;
+  }
 
-            }
+  function mouseDrag(event) {
+      if (drag) {
+          path.add(new paper.Point(event.point));
+          path.smooth();
+      }
+  }
 
-            function mouseDrag(event) {
-                if (drag) {
-                    path.add(new paper.Point(event.point));
-                    path.smooth();
-                }
-            }
+  function mouseDown(event) {
+      //Set  flag to detect mouse drag
+      drag = true;
+      path = new paper.Path();
+      path.strokeColor = 'black';
+      path.strokeWidth = 10;
+      // path.add(new paper.Point(event.layerX, event.layerY));
+      path.add(new paper.Point(event.point));
+  }
 
-            function mouseDown(event) {
-                //Set  flag to detect mouse drag
-                drag = true;
-                path = new paper.Path();
-                path.strokeColor = 'black';
-                path.strokeWidth = 10;
-                // path.add(new paper.Point(event.layerX, event.layerY));
-                path.add(new paper.Point(event.point));
-            }
+  function initPaper() {
+      paper.install(window);
+      paper.setup('myCanvas');
+  }
 
-            function initPaper() {
-                paper.install(window);
-                paper.setup('myCanvas');
+  $scope.deactivate =function (){
+    console.log('deavtivaet!')
+    var tool = new Tool();
+    tool.activate();
+  }
 
-
-            }
-
-            // element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
-            initPaper();
-            var tool = new Tool(); 
-            tool.onMouseDown = mouseDown;
-            tool.onMouseDrag = mouseDrag;
-            tool.onMouseup = mouseUp;
-            tool.activate();
+  initPaper();
+  var tool = new Tool(); 
+  tool.onMouseDown = mouseDown;
+  tool.onMouseDrag = mouseDrag;
+  tool.onMouseup = mouseUp;
+  tool.activate();
 })
