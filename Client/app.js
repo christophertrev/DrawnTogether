56634d17ae8814e721f9ba8ea62657b3922656eb
Christopher Treviño <christophertrev@gmail.com>
2014-12-01 19:38:19 -0800

    Modularized some stuff

--- Client/app.js
+++ Client/app.js
@@ -21,13 +21,11 @@ angular.module('ct-draw',[
     paper.install(window);
     paper.setup('myCanvas');
     var r = new Raster('cat')
-    
     r.position = view.center;
     r.on('load',function(){
-      
-    r.size = new Size(600,400)
-    $scope.baseColor = r.getAverageColor().toString()
-    $scope.$apply()
+      r.size = new Size(600,400)
+      $scope.baseColor = r.getAverageColor().toString()
+      $scope.$apply()
     })
   }
 
@@ -44,9 +42,7 @@ angular.module('ct-draw',[
       radius: 30,
       strokeColor: 'white'
     });
-    
     paper.view.draw();
-
     function onMouseMove(event) {
       console.log(raster.getAverageColor().toString())
       path.fillColor = raster.getAverageColor(event.point);
@@ -56,29 +52,24 @@ angular.module('ct-draw',[
     var tool = new Tool();
     tool.onMouseMove = onMouseMove;
     tool.activate();
-  
   };
 
   initPaper();
-  var tool = new Tool(); 
-  tool.onMouseDown = $scope.mouseDown;
-  tool.onMouseDrag = $scope.mouseDrag;
-  tool.onMouseup = $scope.mouseUp;
-  tool.activate();
 })
 .factory('canvasFuncs', function(){
   var obj = {};
   var drag = false;
   obj.mouseUp = function (event) {
       drag = false;
-  }
+  };
 
   obj.mouseDrag = function (event) {
     if (drag) {
+      // add web socket here to send event 
         path.add(new paper.Point(event.point));
         path.smooth();
     }
-  }
+  };
 
   obj.mouseDown = function(event) {
     drag = true;
@@ -86,6 +77,16 @@ angular.module('ct-draw',[
     path.strokeColor = 'black';
     path.strokeWidth = 10;
     path.add(new paper.Point(event.point));
-  }
-  return obj
+  };
+
+  obj.startDrawing = function (){
+    console.log('drawTime!')
+    var tool = new Tool(); 
+    tool.onMouseDown = obj.mouseDown;
+    tool.onMouseDrag = obj.mouseDrag;
+    tool.onMouseup = obj.mouseUp;
+    tool.activate(); 
+  };
+
+  return obj;
 })
